10414746e0e787124be714584a1377faf5a8a319
김미경 <rmfosem613@gmail.com>
2025-01-22 15:37:20 +0900

    init(react): React 프로젝트 초기화
    React 프로젝트 초기화를 위한 작업을 수행했습니다.
    - Create React App을 사용하여 프로젝트 생성
    - 기본 디렉토리 구조 생성 (src, public)

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+
+// 페이지 컴포넌트
+import LandingPage from '@pages/landing/landingPage';
+import MainPage from '@pages/main/mainPage';
+import LoginPage from '@pages/login/loginPage';
+import VoiceChatPage from '@pages/chat/voiceChatPage';
+import VideoChatPage from '@/pages/chat/videoChatPage';
+import JoinPage from '@pages/login/joinPage';
+import TotalReportPage from '@pages/report/totalReportPage';
+import ChatReportPage from '@pages/report/chatReportPage';
+
+
+import '@assets/styles/font.scss'; // Pretendard 폰트 SCSS 추가
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <RecoilRoot>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route index path="/" element={<LandingPage />} />
+            <Route index path="/main" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/join" element={<JoinPage />} />
+            <Route path="/voice_chat" element={<VoiceChatPage />} />
+            <Route path="/video_chat" element={<VideoChatPage />} />
+            <Route path="/report" element={<TotalReportPage />} />
+            <Route path="/report/detail" element={<ChatReportPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </RecoilRoot>
+  );
 }
 
-export default App
+export default App;
