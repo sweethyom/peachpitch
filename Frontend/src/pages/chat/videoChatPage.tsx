54b7489a57289a50983506e7c79576b8808ba4bc
김미경 <rmfosem613@gmail.com>
2025-01-29 00:28:21 +0900

    feat(video): 키워드 모달창 추가
    1:1 매칭 스몰토크 페이지에도 키워드 모달창을 추가했습니다.

--- Frontend/src/pages/chat/videoChatPage.tsx
+++ Frontend/src/pages/chat/videoChatPage.tsx
@@ -3,7 +3,6 @@ import Footer from '../../components/footer/Footer';
 import styles from './styles/video.module.scss'
 
 import leaveBtn from '../../assets/icons/leave.png'
-import { Link } from 'react-router-dom';
 
 import sstBtn from '../../assets/icons/chat_sst.png'
 import WebcamComponent from '@/components/chat/WebcamComponent';
@@ -12,14 +11,32 @@ import Drawer from '@/components/chat/Drawer';
 import { useState } from 'react';
 
 import RoomLeaveModal from '@/components/modal/RoomLeave';
+import KeywordModal from '@/components/modal/Keyword';
+import RedAlert from '@/components/alert/redAlert';
 
 function videoChatPage() {
 
   /* 대화 나가기 모달창 */
   const [isLeaveOpen, setIsLeaveOpen] = useState(false);
+  const toggleLeave = () => setIsLeaveOpen(!isLeaveOpen);
 
-  const toggleLeave = () => {
-    setIsLeaveOpen(!isLeaveOpen);
+  /* 키워드 모달창 */
+  const [isKeywordOpen, setIsKeywordOpen] = useState(true);
+  const toggleKeyword = () => setIsKeywordOpen(!isKeywordOpen);
+
+  /* 키워드 상태 */
+  const [selectedKeyword, setSelectedKeyword] = useState<string | null>(null);
+
+  /* alert 창 */
+  const [showAlert, setShowAlert] = useState(false);
+
+  /* 시작하기 버튼 클릭 시 */
+  const handleStartClick = () => {
+    if (!selectedKeyword) {
+      setShowAlert(true);
+      return;
+    }
+    setIsKeywordOpen(false); // 키워드가 선택된 경우 모달 닫기
   };
 
   return (
@@ -27,7 +44,7 @@ function videoChatPage() {
 
       {/* 설정 메뉴바 */}
       <div className={styles.menu}>
-        <Drawer />
+        <Drawer selectedKeyword={selectedKeyword} />
       </div>
 
       <div className={styles.chat}>
@@ -35,11 +52,11 @@ function videoChatPage() {
         <div className={styles.chat__header}>
           <p className={styles.chat__header__title}>1:1 매칭 스몰토크</p>
 
-            {/* 대화 나가기 아이콘 */}
-            <img
-              src={leaveBtn}
-              onClick={toggleLeave}
-              className={styles.chat__header__img} />
+          {/* 대화 나가기 아이콘 */}
+          <img
+            src={leaveBtn}
+            onClick={toggleLeave}
+            className={styles.chat__header__img} />
         </div>
 
         {/* 상대방 웹캠 */}
@@ -49,7 +66,7 @@ function videoChatPage() {
           </div>
           <div className={styles.chat__other__bubble}>
             <div className={styles.bubble__left}>
-              여행에 대해 이야기 나누기 좋아요! 최근에 여행 간 곳 중에 가장 기억에 남는 곳이 있으신가요?
+              {selectedKeyword || "여행"}에 대해 이야기 나누기 좋아요! 최근에 가장 기억에 남는 일이 있으신가요?
             </div>
           </div>
         </div>
@@ -76,6 +93,24 @@ function videoChatPage() {
       </div>
 
       <Footer />
+
+      {/* 키워드 모달 */}
+      <KeywordModal
+        isOpen={isKeywordOpen}
+        onClose={toggleKeyword}
+        setSelectedKeyword={setSelectedKeyword}>
+        <div className={styles.btn} onClick={handleStartClick}>시작하기</div>
+      </KeywordModal>
+
+      {/* 키워드 선택안했을 경우 뜨는 alert창 */}
+      {showAlert && (
+        <div style={{ zIndex: 9999 }}>
+          <RedAlert
+            message="키워드를 선택해주세요!"
+            onClose={() => setShowAlert(false)}
+          />
+        </div>
+      )}
 
       {/* 대화 나가기 모달 */}
       <RoomLeaveModal isOpen={isLeaveOpen} onClose={toggleLeave} />
