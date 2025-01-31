7a9e5c9414779884c3285901e42f1ab693cee7fc
최지원 <chjw9559@gmail.com>
2025-01-31 17:37:19 +0900

    feat(auth): JWT 토큰 인증 기능 추가
    JWT 토큰을 활용하여 사용자 인증 기능을 추가했습니다.
    토큰 생성 및 검증 로직을 구현하였습니다.

--- Backend/src/main/java/com/ssafy/peachptich/service/UserServiceImpl.java
+++ Backend/src/main/java/com/ssafy/peachptich/service/UserServiceImpl.java
@@ -1,11 +1,39 @@
 package com.ssafy.peachptich.service;
 
+import com.ssafy.peachptich.dto.request.JoinDTO;
+import com.ssafy.peachptich.entity.User;
 import com.ssafy.peachptich.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void joinProcess(JoinDTO joinDTO){
+        String userEmail = joinDTO.getEmail();
+        String password = joinDTO.getPassword();
+        LocalDate birth = joinDTO.getBirth();
+
+        Boolean isExist = userRepository.existsByEmail(userEmail);
+
+        if (isExist) {
+            return;
+        }
+
+        User data = new User();
+        data.setEmail(userEmail);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setBirth(birth);
+        data.setRole("ROLE_USER");
+
+        userRepository.save(data);
+        System.out.println("successfully data saved!");
+
+    }
 }
