75f8b9d1ec58b803f4230bcdccd617080d35b87e
최지원 <chjw9559@gmail.com>
2025-01-31 17:37:19 +0900

    feat(auth): JWT 토큰 인증 기능 추가
    JWT 토큰을 활용하여 사용자 인증 기능을 추가했습니다.
    토큰 생성 및 검증 로직을 구현하였습니다.

---
+++ Backend/src/main/java/com/ssafy/peachptich/controller/users/UserController.java
@@ -0,0 +1,24 @@
+package com.ssafy.peachptich.controller.users;
+
+import com.ssafy.peachptich.dto.request.JoinDTO;
+import com.ssafy.peachptich.service.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@ResponseBody
+public class UserController {
+    private final UserServiceImpl userServiceimpl;
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO){
+        System.out.println("UserController 입성");
+        userServiceimpl.joinProcess(joinDTO);
+
+        return "join success";
+    }
+
+}
