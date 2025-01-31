992e7a2c96df9b07df5a3e59298610f462103ccf
김민주 <mintminju130@naver.com>
2025-01-31 16:28:07 +0900

    feat(keyword): 랜덤 키워드 반환 기능 추가
    프로젝트 첫 실행 시 DB에 키워드, 힌트 데이터를 넣도록 구현했습니다.
    15개의 랜덤 키워드를 반환하는 기능을 구현했습니다.

--- Backend/src/main/java/com/ssafy/peachptich/entity/Keyword.java
+++ Backend/src/main/java/com/ssafy/peachptich/entity/Keyword.java
@@ -2,19 +2,20 @@ package com.ssafy.peachptich.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Keyword {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long keywordId;
 
-    @Column(length = 30, nullable = false)
+    @Column(length = 30, nullable = false, unique = true)
     private String keyword;
 
+    @ColumnDefault("0")
     private Integer totalCount;
 
     @Builder
