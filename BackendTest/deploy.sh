804be6ae462b733120959496f6028626f9a419ac
정유선 <seon7129@naver.com>
2025-02-03 11:08:28 +0900

    test(backend): docker-compose 파일명 수정
    블루그린 배포 테스트를 위한 파일명 수정

--- BackendTest/deploy.sh
+++ BackendTest/deploy.sh
@@ -2,22 +2,22 @@
 
 #0
 # 이미지 갱신
-sudo docker compose -p blue-8081 -f ./docker-compose.blue8081.yml pull
-sudo docker compose -p green-8082 -f ./docker-compose.green8082.yml pull
+sudo docker compose -p bluegreen-8081 -f ./docker-compose.bluegreen8081.yml pull
+sudo docker compose -p bluegreen-8082 -f ./docker-compose.bluegreen8082.yml pull
 
 #1
-EXIST_GITCHAN=$(sudo docker compose -p blue-8081 -f docker-compose.blue8081.yml ps | grep Up)
+EXIST_GITCHAN=$(sudo docker compose -p blue-8081 -f docker-compose.bluegreen8081.yml ps | grep Up)
 
 if [ -z "$EXIST_GITCHAN" ]; then
         echo "8081 컨테이너 실행"
-        sudo docker compose -p blue-8081 -f /home/ubuntu/docker-compose.blue8081.yml up -d --force-recreate
+        sudo docker compose -p bluegreen-8081 -f ./docker-compose.bluegreen8081.yml up -d --force-recreate
         BEFORE_COLOR="8082"
         AFTER_COLOR="8081"
         BEFORE_PORT=8082
         AFTER_PORT=8081
 else
         echo "8082 컨테이너 실행"
-        sudo docker compose -p green-8082 -f /home/ubuntu/docker-compose.green8082.yml up -d --force-recreate
+        sudo docker compose -p bluegreen-8082 -f ./docker-compose.bluegreen8082.yml up -d --force-recreate
         BEFORE_COLOR="8081"
         AFTER_COLOR="8082"
         BEFORE_PORT=8081
@@ -52,7 +52,7 @@ echo "Deploy Completed!!"
 
 # 4
 echo "$BEFORE_COLOR server down(port:${BEFORE_PORT})"
-sudo docker compose -p ulvan-${BEFORE_COLOR} -f docker-compose.ulvan${BEFORE_COLOR}.yml down
+sudo docker compose -p bluegreen-${BEFORE_COLOR} -f docker-compose.bluegreen${BEFORE_COLOR}.yml down
 
 # 5
 sudo docker image prune -f
