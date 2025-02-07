8d86151f3c24873d9cee8e9b93cec41887f95f97
김민주 <mintminju130@naver.com>
2025-02-07 11:12:16 +0900

    feat(video): 1:1 화상 채팅 웹소켓 방식 접속 추가
    웹소켓을 활용한 1:1 화상 채팅 접속 기능을 추가했습니다.
    매칭 전에는 status,매칭 시 token, status, 대화내역 id, 사용자와 상대방 이름을 반환합니다.

--- Backend/src/main/java/com/ssafy/peachptich/dto/response/RoomResponseDto.java
+++ Backend/src/main/java/com/ssafy/peachptich/dto/response/RoomResponseDto.java
@@ -2,10 +2,15 @@ package com.ssafy.peachptich.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 public class RoomResponseDto {
     private String token;
+    private String status;
     private Long historyId;
+    private String userName; //나의 이름
+    private String matchedUserName; //상대방 이름
 }
