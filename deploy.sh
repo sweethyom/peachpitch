178caa7ecf1d5923d6fd755cb82ed4599ff7ac25
정유선 <seon7129@naver.com>
2025-02-08 15:02:40 +0900

    test(backend): docker-compose.yml, deploy.sh 파일 수정
    django 포트를 8083, 8084로 변경

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 echo "새로운 이미지를 가져옵니다..."
 sudo docker compose -p bluegreen-8081 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8081.yml pull
 sudo docker compose -p bluegreen-8082 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8082.yml pull
-sudo docker compose -p bluegreen-django-8081 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8081.yml pull
-sudo docker compose -p bluegreen-django-8082 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8082.yml pull
+sudo docker compose -p bluegreen-8083 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8083.yml pull
+sudo docker compose -p bluegreen-8084 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8084.yml pull
 sudo docker compose -p my-react -f /home/ubuntu/S12P11D201/test/docker-compose.yml pull
 
 # 1. 실행 중인 컨테이너 확인 및 중복 제거
@@ -24,8 +24,8 @@ function remove_existing_container() {
 # Blue-Green 환경에 따라 컨테이너 이름 설정
 BLUE_S_CONTAINER="bluegreen-8081"
 GREEN_S_CONTAINER="bluegreen-8082"
-BLUE_D_CONTAINER="bluegreen-django-8081"
-GREEN_D_CONTAINER="bluegreen-django-8082"
+BLUE_D_CONTAINER="bluegreen-8083"
+GREEN_D_CONTAINER="bluegreen-8084"
 
 # Blue-Green 배포 환경 전환
 EXIST_GITCHAN=$(sudo docker compose -p blue-8081 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8081.yml ps | grep Up)
@@ -35,21 +35,29 @@ if [ -z "$EXIST_GITCHAN" ]; then
     remove_existing_container $BLUE_S_CONTAINER
     remove_existing_container $BLUE_D_CONTAINER
     sudo docker compose -p bluegreen-8081 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8081.yml up -d --force-recreate --remove-orphans
-    sudo docker compose -p bluegreen-django-8081 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8081.yml up -d --force-recreate --remove-orphans
-    BEFORE_COLOR="8082"
-    AFTER_COLOR="8081"
-    BEFORE_PORT=8082
-    AFTER_PORT=8081
+    sudo docker compose -p bluegreen-8083 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8083.yml up -d --force-recreate --remove-orphans
+    BEFORE_S_COLOR="8082"
+    AFTER_S_COLOR="8081"
+    BEFORE_S_PORT=8082
+    AFTER_S_PORT=8081
+    BEFORE_D_COLOR="8084"
+    AFTER_D_COLOR="8083"
+    BEFORE_D_PORT=8084
+    AFTER_D_PORT=8083
 else
     echo "8082(그린) 환경이 실행되지 않음. 8082 환경 실행..."
     remove_existing_container $GREEN_S_CONTAINER
     remove_existing_container $GREEN_D_CONTAINER
     sudo docker compose -p bluegreen-8082 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8082.yml up -d --force-recreate --remove-orphans
-    sudo docker compose -p bluegreen-django-8082 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8082.yml up -d --force-recreate --remove-orphans
-    BEFORE_COLOR="8081"
-    AFTER_COLOR="8082"
-    BEFORE_PORT=8081
-    AFTER_PORT=8082
+    sudo docker compose -p bluegreen-8084 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8084.yml up -d --force-recreate --remove-orphans
+    BEFORE_S_COLOR="8081"
+    AFTER_S_COLOR="8082"
+    BEFORE_S_PORT=8081
+    AFTER_S_PORT=8082
+    BEFORE_D_COLOR="8084"
+    AFTER_D_COLOR="8083"
+    BEFORE_D_PORT=8084
+    AFTER_D_PORT=8083
 fi
 
 # React 서버 실행 (중복된 React 컨테이너 제거)
@@ -58,7 +66,8 @@ echo "React 서버 실행..."
 remove_existing_container $REACT_CONTAINER
 sudo docker compose -p my-react -f /home/ubuntu/S12P11D201/test/docker-compose.yml up -d --force-recreate --remove-orphans
 
-echo "${AFTER_COLOR} 서버가 실행되었습니다 (포트: ${AFTER_PORT})"
+echo "${AFTER_S_COLOR} springboot 서버가 실행되었습니다 (포트: ${AFTER_S_PORT})"
+echo "${AFTER_D_COLOR} django 서버가 실행되었습니다 (포트: ${AFTER_D_PORT})"
 
 # 2. 새 버전 서버 응답 확인
 # for cnt in `seq 1 10`;
@@ -95,25 +104,26 @@ echo "Nginx 설정 파일 업데이트..."
 NGINX_CONF_DIR="/home/ubuntu/nginx_conf"
 NGINX_CONF_PATH="${NGINX_CONF_DIR}/default.conf"
 
-if [ "$AFTER_COLOR" == "8081" ]; then
-    echo "Blue 환경(8081)으로 Nginx 설정 파일 교체..."
-    sudo cp ${NGINX_CONF_DIR}/default8081.conf $NGINX_CONF_PATH
+if [ "$AFTER_S_COLOR" == "8081" ]; then
+    echo "Blue 환경으로 Nginx 설정 파일 교체..."
+    sudo cp ${NGINX_CONF_DIR}/defaultblue.conf $NGINX_CONF_PATH
 else
-    echo "Green 환경(8082)으로 Nginx 설정 파일 교체..."
-    sudo cp ${NGINX_CONF_DIR}/default8082.conf $NGINX_CONF_PATH
+    echo "Green 환경으로 Nginx 설정 파일 교체..."
+    sudo cp ${NGINX_CONF_DIR}/defaultgreen.conf $NGINX_CONF_PATH
 fi
 
 # 기존 포트를 새로운 포트로 교체
-sudo sed -i "s/server 172.20.0.1:${BEFORE_PORT};/server 172.20.0.1:${AFTER_PORT};/" $NGINX_CONF_PATH
+#sudo sed -i "s/server 172.20.0.1:${BEFORE_S_PORT};/server 172.20.0.1:${AFTER_S_PORT};/" $NGINX_CONF_PATH
 
 # Nginx 컨테이너에 업데이트된 설정 파일 복사
 sudo docker cp $NGINX_CONF_PATH my-nginx:/etc/nginx/conf.d/default.conf
 sudo docker restart my-nginx
 
 # 4. 이전 환경(블루 서버) 중지 및 정리 (중복 방지)
-echo "${BEFORE_COLOR} 서버 중지 (포트: ${BEFORE_PORT})"
-sudo docker compose -p bluegreen-${BEFORE_COLOR} -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen${BEFORE_COLOR}.yml down
-sudo docker compose -p bluegreen-django-${BEFORE_COLOR} -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen${BEFORE_COLOR}.yml down
+echo "${BEFORE_S_COLOR} springboot 서버 중지 (포트: ${BEFORE_S_PORT})"
+echo "${BEFORE_D_COLOR} django 서버 중지 (포트: ${BEFORE_D_PORT})"
+sudo docker compose -p bluegreen-${BEFORE_S_COLOR} -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen${BEFORE_S_COLOR}.yml down
+sudo docker compose -p bluegreen-${BEFORE_D_COLOR} -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen${BEFORE_D_COLOR}.yml down
 
 # 5. 사용되지 않는 Docker 이미지 정리
 echo "사용되지 않는 Docker 이미지 삭제 중..."
