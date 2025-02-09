9966241be443fe3d7c957ce517618e97bd7f4b9f
정유선 <seon7129@naver.com>
2025-02-09 15:34:54 +0900

    test(backend): deploy.sh 파일 수정
    sudo 삭제
    
    
    Former-commit-id: 539d91574398496f34a2606aa9588784d70f74b5

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 # 0. 이미지 갱신
 echo "새로운 이미지를 가져옵니다..."
-sudo docker compose -p bluegreen-8081 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8081.yml pull
-sudo docker compose -p bluegreen-8082 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8082.yml pull
-sudo docker compose -p bluegreen-8083 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8083.yml pull
-sudo docker compose -p bluegreen-8084 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8084.yml pull
-sudo docker compose -p my-react -f /home/ubuntu/S12P11D201/test/docker-compose.yml pull
+docker-compose -p bluegreen-8081 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8081.yml pull
+docker-compose -p bluegreen-8082 -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen8082.yml pull
+docker-compose -p bluegreen-8083 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8083.yml pull
+docker-compose -p bluegreen-8084 -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen8084.yml pull
+docker-compose -p my-react -f /home/ubuntu/S12P11D201/test/docker-compose.yml pull
 
 # 1. 실행 중인 컨테이너 확인 및 중복 제거
 function remove_existing_container() {
@@ -15,8 +15,8 @@ function remove_existing_container() {
     existing_container=$(sudo docker ps -a --filter "name=$container_name" --format "{{.ID}}")
     if [ ! -z "$existing_container" ]; then
         echo "기존 컨테이너($container_name)가 존재합니다. 삭제 중..."
-        sudo docker stop $container_name >/dev/null 2>&1
-        sudo docker rm $container_name >/dev/null 2>&1
+        docker stop $container_name >/dev/null 2>&1
+        docker rm $container_name >/dev/null 2>&1
         echo "기존 컨테이너($container_name) 삭제 완료."
     fi
 }
@@ -64,7 +64,7 @@ fi
 REACT_CONTAINER="my-react"
 echo "React 서버 실행..."
 remove_existing_container $REACT_CONTAINER
-sudo docker compose -p my-react -f /home/ubuntu/S12P11D201/test/docker-compose.yml up -d --force-recreate --remove-orphans
+docker-compose -p my-react -f /home/ubuntu/S12P11D201/test/docker-compose.yml up -d --force-recreate --remove-orphans
 
 echo "${AFTER_S_COLOR} springboot 서버가 실행되었습니다 (포트: ${AFTER_S_PORT})"
 echo "${AFTER_D_COLOR} django 서버가 실행되었습니다 (포트: ${AFTER_D_PORT})"
@@ -106,26 +106,26 @@ NGINX_CONF_PATH="${NGINX_CONF_DIR}/default.conf"
 
 if [ "$AFTER_S_COLOR" == "8081" ]; then
     echo "Blue 환경으로 Nginx 설정 파일 교체..."
-    sudo cp ${NGINX_CONF_DIR}/defaultblue.conf $NGINX_CONF_PATH
+    cp ${NGINX_CONF_DIR}/defaultblue.conf $NGINX_CONF_PATH
 else
     echo "Green 환경으로 Nginx 설정 파일 교체..."
-    sudo cp ${NGINX_CONF_DIR}/defaultgreen.conf $NGINX_CONF_PATH
+    cp ${NGINX_CONF_DIR}/defaultgreen.conf $NGINX_CONF_PATH
 fi
 
 # 기존 포트를 새로운 포트로 교체
 #sudo sed -i "s/server 172.20.0.1:${BEFORE_S_PORT};/server 172.20.0.1:${AFTER_S_PORT};/" $NGINX_CONF_PATH
 
 # Nginx 컨테이너에 업데이트된 설정 파일 복사
-sudo docker cp $NGINX_CONF_PATH my-nginx:/etc/nginx/conf.d/default.conf
-sudo docker restart my-nginx
+docker cp $NGINX_CONF_PATH my-nginx:/etc/nginx/conf.d/default.conf
+docker restart my-nginx
 
 # 4. 이전 환경(블루 서버) 중지 및 정리 (중복 방지)
 echo "${BEFORE_S_COLOR} springboot 서버 중지 (포트: ${BEFORE_S_PORT})"
 echo "${BEFORE_D_COLOR} django 서버 중지 (포트: ${BEFORE_D_PORT})"
-sudo docker compose -p bluegreen-${BEFORE_S_COLOR} -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen${BEFORE_S_COLOR}.yml down
-sudo docker compose -p bluegreen-${BEFORE_D_COLOR} -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen${BEFORE_D_COLOR}.yml down
+docker-compose -p bluegreen-${BEFORE_S_COLOR} -f /home/ubuntu/S12P11D201/BackendTest/docker-compose.bluegreen${BEFORE_S_COLOR}.yml down
+docker-compose -p bluegreen-${BEFORE_D_COLOR} -f /home/ubuntu/S12P11D201/AITest/docker-compose.bluegreen${BEFORE_D_COLOR}.yml down
 
 # 5. 사용되지 않는 Docker 이미지 정리
 echo "사용되지 않는 Docker 이미지 삭제 중..."
-sudo docker image prune -f
+docker image prune -f
 
