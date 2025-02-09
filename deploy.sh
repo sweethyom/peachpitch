33d45bd7e87d9c5882439c23c1cd7e1cb262ad86
정유선 <seon7129@naver.com>
2025-02-09 22:14:04 +0900

    test(backend): deploy.sh 파일 수정
    dash는 == 제공 안함. =로 변경
    
    
    Former-commit-id: fdf3caf968d06fa5d219af030128fc0855c0bfa2

--- deploy.sh
+++ deploy.sh
@@ -104,7 +104,7 @@ echo "Nginx 설정 파일 업데이트..."
 NGINX_CONF_DIR="/home/ubuntu/nginx_conf"
 NGINX_CONF_PATH="${NGINX_CONF_DIR}/default.conf"
 
-if [ "$AFTER_S_COLOR" == "8081" ]; then
+if [ "$AFTER_S_COLOR" = "8081" ]; then
     echo "Blue 환경으로 Nginx 설정 파일 교체..."
     cp ${NGINX_CONF_DIR}/defaultblue.conf $NGINX_CONF_PATH
 else
