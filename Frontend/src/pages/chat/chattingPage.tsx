de8bc88720e45c775474c8acf4b399a8c54435bb
김미경 <rmfosem613@gmail.com>
2025-02-10 13:45:23 +0900

    test(frontend): typescript 타입 에러 수정
    타입 에러들을 수정했습니다.

--- Frontend/src/pages/chat/chattingPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function chattingPage() {
-  return (
-    <div>chattingPage</div>
-  )
-}
-
-export default chattingPage
--- Frontend/src/pages/report/reportPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function reportPage() {
-  return (
-    <div>reportPage</div>
-  )
-}
-
-export default reportPage
