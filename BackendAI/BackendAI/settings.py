6ed046a9d4a1ce138e4fe6de3afa113f11f6333d
정유선 <seon7129@naver.com>
2025-02-10 11:11:34 +0900

    test(통합): docker-compose, Dockerfile 추가
    통합 시도 중...

--- BackendAI/BackendAI/settings.py
+++ BackendAI/BackendAI/settings.py
@@ -36,9 +36,10 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': f"redis://{os.environ.get('REDIS_HOST', 'localhost')}:{os.environ.get('REDIS_PORT', '6379')}/1",
+        'LOCATION': 'redis://43.201.5.129:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'PASSWORD': None
         }
     }
 }
