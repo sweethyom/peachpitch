1e407a95c1aad5d6dd5da15241b25177c0f632d7
김민주 <mintminju130@naver.com>
2025-02-12 16:10:37 +0900

    fix(item): 첫 실행시 item 데이터 추가
    처음 실행할 때 db의 item에 coupon이 없으면 coupon 정보를 넣습니다.

--- Backend/src/main/java/com/ssafy/peachptich/global/config/DataLoader.java
+++ Backend/src/main/java/com/ssafy/peachptich/global/config/DataLoader.java
@@ -1,8 +1,10 @@
 package com.ssafy.peachptich.global.config;
 
 import com.ssafy.peachptich.entity.Hint;
+import com.ssafy.peachptich.entity.Item;
 import com.ssafy.peachptich.entity.Keyword;
 import com.ssafy.peachptich.repository.HintRepository;
+import com.ssafy.peachptich.repository.ItemRepository;
 import com.ssafy.peachptich.repository.KeywordRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +18,17 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class CSVDataLoader implements ApplicationListener<ApplicationReadyEvent> {
+public class DataLoader implements ApplicationListener<ApplicationReadyEvent> {
     private final HintRepository hintRepository;
     private final KeywordRepository keywordRepository;
+    private final ItemRepository itemRepository;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
+
+        // item 데이터 삽입
+        insertDefaultItems();
+
         // Keyword 저장 (존재하면 찾고, 없으면 예외)
         if (hintRepository.count() > 0) {
             log.info("데이터가 이미 로드되었습니다. CSV 파일을 다시 읽지 않습니다.");
@@ -44,8 +51,8 @@ public class CSVDataLoader implements ApplicationListener<ApplicationReadyEvent>
                 // Keyword 없으면 생성
                 Keyword keyword = keywordRepository.findByKeyword(keywordName)
                         .orElseGet(() -> keywordRepository.save(Keyword.builder()
-                        .keyword(keywordName)
-                        .build()));
+                                .keyword(keywordName)
+                                .build()));
 
                 // Hint 저장
                 for (int i = 1; i < columns.length; i++) {
@@ -62,6 +69,24 @@ public class CSVDataLoader implements ApplicationListener<ApplicationReadyEvent>
             log.info("CSV 데이터가 성공적으로 저장되었습니다.");
         } catch (IOException e) {
             log.error("CSV 파일을 읽는 중 오류 발생: ", e);
+        }
+    }
+
+    private void insertDefaultItems() {
+        insertItem("이용권", 0, Item.ItemType.FREE);
+        insertItem("이용권", 1000, Item.ItemType.PAID);
+    }
+
+    private void insertItem(String name, Integer price, Item.ItemType type) {
+        if (!itemRepository.existsByNameAndType(name, type)) {
+            itemRepository.save(Item.builder()
+                    .name(name)
+                    .price(price)
+                    .type(type)
+                    .build());
+            log.info("Item 추가됨: name={}, price={}, type={}", name, price, type);
+        } else {
+            log.info("이미 존재하는 Item: name={}, type={}", name, type);
         }
     }
 }
