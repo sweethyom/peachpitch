4a5348c2177aeaa1a113d8d315454efdf7167615
김민주 <mintminju130@naver.com>
2025-02-13 09:58:49 +0900

    feat(video): 웹소켓 주소 변경, 키워드 선택 기다리는 문구 추가
    웹소켓 엔드포인트를 변경했습니다.
    다른 사람의 키워드 선택을 기다리는 문구를 추가했습니다.

--- Backend/src/main/java/com/ssafy/peachptich/global/config/security/SecurityConfig.java
+++ Backend/src/main/java/com/ssafy/peachptich/global/config/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
                                            AuthenticationConfiguration authenticationConfiguration) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-                                .requestMatchers("/ws/**", "/ws/room/**").permitAll() // WebSocket 엔드포인트
+                                .requestMatchers("/api/ws/**").permitAll() // WebSocket 엔드포인트
                                 .requestMatchers("/pub/**", "/sub/**").permitAll() // STOMP 메시징 경로
                                 .requestMatchers("/api/main/**", "/api/index", "/api/users/login", "/api/users/signup", "/api/pay/ready", "/api/pay/completed",
                                         "/api/chat/ai/keywords/**", "/api/chat/ai/check", "/api/users/coupon/**", "/error", "/api/chat/report/**", "/api/users/check",
