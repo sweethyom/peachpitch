dea19661038079f2168bd2155e85b70b62d92b7c
이한나 <giha0168@gmail.com>
2025-02-13 14:39:23 +0900

    feat(refineAI): 대화 텍스트 정제 및 질문 답변 개수 업데이트 모델 생성
    대화 텍스트 정제 및 질문 답변 개수 업데이트 모델을 만들었습니다.
    reportAI, wordAI가 동기 방식으로 작동 되게 만들었습니다.

--- BackendAI/reportAI/views.py
+++ BackendAI/reportAI/views.py
@@ -5,8 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.urls import path
 import openai
 import json
-import redis
-import time
+
 from .models import Chat, ChatReport, ChatHistory, User
 from django.conf import settings
 
@@ -22,29 +21,12 @@ def generate_report(request):
         if not history_id:
             return JsonResponse({"error": "history_id가 필요합니다."}, status=400)
         
-        # Redis가 대화 내역에 남아있는가 (대화 저장시 사용한 key 규칙에 맞게 수정하라는데..! 효미언니 물어보기)
-        redis_client = redis.Redis(
-            host=settings.REDIS_HOST,
-            port=settings.REDIS_PORT,
-            db=getattr(settings, 'REDIS_DB', 0)
-        )
-        redis_key = f'chat:{history_id}:messages'
-
-        # Polling 시작 (30초 대기로 가겠습니다.)
-        timeout = 30
-        start_time = time.time()
-
-        while redis_client.exists(redis_key):
-            if time.time() - start_time > timeout:
-                return JsonResponse({"error": "대기 시간 초과되었습니다. 나중에 다시 시도해주세요."})
-            time.sleep(1)   # 1초 간격으로 Redis 상태 확인, 부하 가면 수정
+        # ChatHistory 객체를 가져옴
+        history = get_object_or_404(ChatHistory, pk=history_id)
         
-        # Redis가 비었을 때 모델 구동 시작, history_id에에 연결된 Chat 데이터 가져오기
-        chats = Chat.objects.filter(history_id=history_id)
-        if not chats.exists():
-            return JsonResponse({"error": "해당 histroy_id에 대한 채팅 데이터가 없습니다."})
+        # history_id에 연결된 모든 Chat 메시지 조회
+        chats = Chat.objects.filter(history=history)
         
-
         # 여러 개의 대화 내용을 합치기
         conversation_data = "\n".join(chat.content for chat in chats if chat.content)
 
