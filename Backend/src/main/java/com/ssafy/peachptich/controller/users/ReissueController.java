a2e01539d0193cb1ad9738f56faa583108248a90
김미경 <51354302+rmfosem613@users.noreply.github.com>
2025-02-14 14:58:17 +0900

    Merge branch 'develop' of https://lab.ssafy.com/s12-webmobile1-sub1/S12P11D201 into develop
    # Conflicts:
    #	Backend/src/main/java/com/ssafy/peachptich/controller/chat/ChatController.java
    #	Backend/src/main/java/com/ssafy/peachptich/controller/users/ReissueController.java

--- Backend/src/main/java/com/ssafy/peachptich/controller/users/ReissueController.java
+++ Backend/src/main/java/com/ssafy/peachptich/controller/users/ReissueController.java
@@ -87,7 +87,7 @@ public class ReissueController {
         // refreshRepository.deleteByRefresh(refresh);
         // addToken(userEmail, newRefresh, 86400000L);
 
-        addToken("RT:AT:" + userEmail, newAccess, 18000000L);
+        addToken("RT:AT:" + userEmail, newAccess, 600000L);
         addToken("RT:RT:" + userEmail, newRefresh, 86400000L);
 
         // response
@@ -115,8 +115,7 @@ public class ReissueController {
         redisTemplate.opsForValue().set(
                 key,       // key 값
                 value,                // value
-//                System.currentTimeMillis() + expiredMs,     // 만료 시간
-                expiredMs,
+                System.currentTimeMillis() + expiredMs,     // 만료 시간
                 TimeUnit.MICROSECONDS
         );
     }
