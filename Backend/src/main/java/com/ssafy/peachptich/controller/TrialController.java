d479d20deca4c1d11982509d0c4be330fe610657
최지원 <chjw9559@gmail.com>
2025-02-17 16:57:38 +0900

    docs: API 문서를 위한 Swagger configuration 추가
    API 문서를 위해 Swagger configuration을 추가하였습니다.

--- Backend/src/main/java/com/ssafy/peachptich/controller/TrialController.java
+++ Backend/src/main/java/com/ssafy/peachptich/controller/TrialController.java
@@ -3,6 +3,11 @@ package com.ssafy.peachptich.controller;
 import com.ssafy.peachptich.dto.request.TrialRequest;
 import com.ssafy.peachptich.dto.response.TrialResponse;
 import com.ssafy.peachptich.service.TrialService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/trial")
+@Tag(name = "TrialController", description = "무료 이용 관련 컨트롤러")
 public class TrialController {
 
     private final TrialService trialService;
@@ -22,6 +28,10 @@ public class TrialController {
     }
 
     @PostMapping("/check")
+    @Operation(summary = "무료 이용 체크", description = "사용자의 무료 이용 가능 여부를 확인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 확인됨")
+    })
     public ResponseEntity<TrialResponse> checkTrial(@RequestBody TrialRequest request) {
         return ResponseEntity.ok(trialService.checkTrialAccess(request.getFingerprint()));
     }
