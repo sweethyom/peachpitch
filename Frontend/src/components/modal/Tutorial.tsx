fca5083cc7ae38bf0ddf2b6a272ed0409a5bf14c
김미경 <rmfosem613@gmail.com>
2025-02-18 18:02:02 +0900

    feat(assets): 튜토리얼 gif 수정

--- Frontend/src/components/modal/Tutorial.tsx
+++ Frontend/src/components/modal/Tutorial.tsx
@@ -5,13 +5,13 @@ import leftBtn from "@/assets/icons/modal__left.png";
 import rightBtn from "@/assets/icons/modal__right.png";
 import { useState } from "react";
 
-import ai1Img from "@/assets/tutorial/ai1.mp4";
-import ai2Img from "@/assets/tutorial/ai2.mp4";
-import ai3Img from "@/assets/tutorial/ai3.mp4";
-import ai4Img from "@/assets/tutorial/ai4.mp4";
-import peer1Img from "@/assets/tutorial/peer1.mp4";
-import peer2Img from "@/assets/tutorial/peer2.mp4";
-import peer4Img from "@/assets/tutorial/peer4.mp4";
+import ai1Img from "@/assets/tutorial/ai1.gif";
+import ai2Img from "@/assets/tutorial/ai2.gif";
+import ai3Img from "@/assets/tutorial/ai3.gif";
+import ai4Img from "@/assets/tutorial/ai4.gif";
+import peer1Img from "@/assets/tutorial/peer1.gif";
+import peer2Img from "@/assets/tutorial/peer2.gif";
+import peer4Img from "@/assets/tutorial/peer4.gif";
 
 type ModalProps = {
     isOpen: boolean; // 모달 열림 상태
@@ -27,8 +27,8 @@ function Tutorial({ isOpen, onClose }: ModalProps) {
     const tutorials: { id: number; src: string; type: "ai" | "peer" }[] = [
         { id: 1, src: ai1Img, type: "ai" },
         { id: 2, src: ai2Img, type: "ai" },
-        { id: 3, src: ai3Img, type: "ai" },
         { id: 4, src: ai4Img, type: "ai" },
+        { id: 3, src: ai3Img, type: "ai" },
         { id: 5, src: peer1Img, type: "peer" },
         { id: 6, src: peer2Img, type: "peer" },
         { id: 7, src: peer4Img, type: "peer" },
@@ -44,33 +44,33 @@ function Tutorial({ isOpen, onClose }: ModalProps) {
         {
             title: "AI와 진행하는 스몰토크 연습 (2)",
             description: "AI와의 대화를 통해 자연스럽게 스몰토킹을 연습할 수 있습니다.",
-            content: "2. 녹음 파일을 AI에게 보내면 정확도 높은 스몰토킹을 진행할 수 있습니다.",
+            content: "2. 키워드를 선택해 주제에 맞는 스몰토크를 진행합니다.",
         },
         {
             title: "AI와 진행하는 스몰토크 연습 (3)",
-            description: "대화가 힘들다면 힌트를 활용해 다른 주제로 전환할 수 있습니다.",
+            description: "실제 AI와 진행한 스몰토크 예시",
+            content: "어머! 코를 자꾸 만지는 습관이 있군요",
+        },
+        {
+            title: "AI와 진행하는 스몰토크 연습 (4)",
+            description: "대화가 힘들다면 힌트를를 활용해 해결해 보세요!",
             content: "3. AI와 주어진 총 10번의 문답을 주고 받을 수 있습니다.",
         },
         {
             title: "익명의 사용자와 1:1 매칭 (1)",
             description: "1:1 매칭을 통해 자연스럽게 대화를 나누고 소통 능력을 키울 수 있습니다.",
-            content: "1. 메인화면에서 [1:1 매칭 스몰토킹] 모드를 선택하세요.",
+            content: "1. 매칭이 되었다면 메인화면에서 [1:1 매칭 스몰토킹] 모드를 선택하세요.",
         },
         {
             title: "익명의 사용자와 1:1 매칭 (2)",
             description: "대화할 키워드를 선택하면 대화가 시작됩니다.",
-            content: "2. 말문이 막힐 땐 힌트를 이용해보세요!",
+            content: "2. 어떤 상대와 만날지 두근두근 하네요!",
         },
         {
-            title: "AI가 분석해주는 나만의 리포트 (1)",
-            description: "스몰토킹이 끝난 후 AI가 분석한 리포트를 확인할 수 있습니다.",
-            content: "1. 리포트에서는 총 대화 시간, 평균 공백 시간, 대화 주도권 등을 분석합니다.",
-        },
-        {
-            title: "AI가 분석해주는 나만의 리포트 (2)",
-            description: "AI가 제공하는 피드백을 통해 자신의 대화 습관을 개선할 수 있습니다.",
-            content: "2. 리포트 상세에서 대화 기록과 AI 피드백을 확인할 수 있습니다.",
-        },
+            title: "익명의 사용자와 1:1 매칭 (3)",
+            description: "스몰토킹이 끝난 후 상대에 대한 평가가 진행됩니다.",
+            content: "3. 상대의 리포트에 전해지는 만큼 평가에 신중해주세요!",
+        }
     ];
 
     // 현재 페이지의 데이터
@@ -121,21 +121,16 @@ function Tutorial({ isOpen, onClose }: ModalProps) {
 
                     <div className={styles.wrap}>
                         {/* 설명 텍스트 */}
-                        <p className={styles.modal__contents__label}>{currentPageData.content}</p>
 
                         {/* 현재 페이지의 영상 */}
-                        <video
-                            src={currentTutorial.src}
-                            width={"200px"}
-                            muted
-                            autoPlay
-                            loop
-                            playsInline
-                            preload="auto"
-                            controls
-                            className={styles.wrap__video} />
+                        <div className={styles.wrap__div}>
+                            <img src={currentTutorial.src} className={styles.wrap__gif} />
+                        </div>
+
+                        <p className={styles.modal__contents__label}>{currentPageData.content}</p>
 
                     </div>
+
                     {/* 오른쪽 버튼 - 마지막 페이지에서는 보이지 않음 */}
                     <div
                         className={styles.modal__contents__btn}
