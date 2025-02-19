2971268c407800965e3b69bdfc222d9d1e0a164d
김미경 <rmfosem613@gmail.com>
2025-02-19 10:11:33 +0900

    feat(main): 로그아웃 수정 및 entity 수정

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import ScrollToTop from "@/components/ScrollToTop";
