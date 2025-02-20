551d580369586783c39ad7133daa5d3bb922e643
김민주 <mintminju130@naver.com>
2025-02-20 13:47:31 +0900

    test(통합): 자동종료 시간 늘림

--- Frontend6/src/pages/chat/videoChatPage.tsx
+++ Frontend6/src/pages/chat/videoChatPage.tsx
@@ -527,7 +527,7 @@ const VideoChatPage: React.FC = () => {
                     console.error("STOMP client가 연결되어 있지 않습니다 (자동 종료).");
                 }
             }
-        }, 50000);
+        }, 80000);
 
         return () => clearTimeout(autoEndTimeout);
     }, [session, token, isSessionClosed, sessionId]);
