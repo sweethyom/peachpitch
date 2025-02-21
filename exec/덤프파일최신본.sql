2cb4be3e22c343836ed2886e8c349420ecfac1c3
정유선 <seon7129@naver.com>
2025-02-21 09:50:15 +0900

    docs(readme): 효미언니 정보 업데이트, sql 업데이트
    Former-commit-id: 75a175f8c0d0afab6d682ed41958e2d4a2be6113

---
+++ exec/덤프파일최신본.sql
@@ -0,0 +1,5 @@
+insert chat(chat_id, created_at, history_id, user_id, content) values(1, "2025-02-21 09:00:00", 1, 1, "평소에 대화를 많이 하세요?");
+insert chat(chat_id, created_at, history_id, user_id, content) values(2, "2025-02-21 09:00:00", 1, 1, "대화를 하는게 왜 두려울까요?");
+insert chat(chat_id, created_at, history_id, user_id, content) values(3, "2025-02-21 09:00:00", 1, 1, "시간가는 줄 모르겠어요");
+insert chat(chat_id, created_at, history_id, user_id, content) values(4, "2025-02-21 09:00:00", 1, 1, "이렇게 대화하니 편안해요");
+insert chat(chat_id, created_at, history_id, user_id, content) values(5, "2025-02-21 09:00:00", 1, 1, "AI 서비스도 은근 재미있지 않아요?");
